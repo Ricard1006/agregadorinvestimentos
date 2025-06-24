93b30d057e94f48631bad68ecc0cbb93c4146159
Ricardo Neto <ricardofukushimaneto@gmail.com>
2025-06-24 16:20:37 -0300

    mock_2

--- src/test/java/tech/buildrun/agregadorinvestimentos/controller/UserControllerTest.java
+++ src/test/java/tech/buildrun/agregadorinvestimentos/controller/UserControllerTest.java
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tech.buildrun.agregadorinvestimentos.entity.User;
 import tech.buildrun.agregadorinvestimentos.repository.UserRepository;
 import tech.buildrun.agregadorinvestimentos.service.UserService;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
@@ -25,7 +33,26 @@ class UserControllerTest {
     class createUser{
         @Test
         @DisplayName("Should create a user succes")
+        
         void should() {
+
+            //Arange
+            var user = new User(
+                    UUID.randomUUID(),
+                    "username",
+                    "email@email.com",
+                    "password",
+                    Instant.now(),
+                    null
+            );
+            doThrow(new RuntimeException()).when(userRepository).save(any());
+            var input = new CreateUserDto(
+                    "username",
+                    "email@email.com",
+                    "123"
+            );
+            //Act
+            assertThrows(RuntimeException.class, () -> userService.createUser(input));
         }
     }
 
